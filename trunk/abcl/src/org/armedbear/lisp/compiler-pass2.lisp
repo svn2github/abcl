svn2github/abcl

commit 774025e83e5ecffbe2fbe765090f4170e2fa3649

Followup to r11467: STREAM wasn't in the original list of types.
Found by: mevenson


git-svn-id: http://abcl.org/svn@11482 1c010e3e-69d0-11dd-93a8-456734b0d56f

trunk/abcl/src/org/armedbear/lisp/compiler-pass2.lisp
@@ -482,9 +482,9 @@
     (or
      (when (fixnum-type-p declared-type) 'FIXNUM)
      (find-if #'(lambda (type) (eq type declared-type))
-	      `(SYMBOL CHARACTER CONS HASH-TABLE STREAM))
+	      '(SYMBOL CHARACTER CONS HASH-TABLE))
      (find-if #'(lambda (type) (subtypep declared-type type)) 
-	      `(STRING VECTOR)))))
+	      '(STRING VECTOR)))))
 
 
 (defknown generate-type-check-for-variable (t) t)

commit 24025b28b1589804832c7b6a8bc7008711e3b010

Fix READ-BYTE.ERROR.5 (interpreted version) which was broken since r11482.
git-svn-id: http://abcl.org/svn@11493 1c010e3e-69d0-11dd-93a8-456734b0d56f

trunk/abcl/src/org/armedbear/lisp/compiler-pass2.lisp
@@ -484,7 +484,7 @@
      (find-if #'(lambda (type) (eq type declared-type))
 	      '(SYMBOL CHARACTER CONS HASH-TABLE))
      (find-if #'(lambda (type) (subtypep declared-type type)) 
-	      '(STRING VECTOR)))))
+	      '(STRING VECTOR STREAM)))))
 
 
 (defknown generate-type-check-for-variable (t) t)