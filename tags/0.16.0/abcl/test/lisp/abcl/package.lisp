51acc98ceedaab877cd910df3190ee4b9252dc6c
ehuelsmann <ehuelsmann@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2009-09-05 09:35:25 +0000

    Tag 0.16.0.
    git-svn-id: http://abcl.org/svn@12131 1c010e3e-69d0-11dd-93a8-456734b0d56f

---
+++ tags/0.16.0/abcl/test/lisp/abcl/package.lisp
@@ -0,0 +1,27 @@
+(defpackage #:abcl.test.lisp 
+  (:use #:cl #:abcl-rt)
+  (:export #:run))
+(in-package #:abcl.test.lisp)
+
+(defvar *abcl-lisp-test-directory* 
+  (pathname (directory-namestring *load-truename*))
+  "The directory in which the ABCL test source files are located.")
+
+(defun run ()
+  "Run the Lisp test suite for ABCL."
+
+  (let ((*default-pathname-defaults* *abcl-lisp-test-directory*))
+    (rem-all-tests)
+
+    (load "test-utilities.lisp")
+
+    (load "compiler-tests.lisp")
+    (load "condition-tests.lisp")
+    (load "file-system-tests.lisp")
+    (load "java-tests.lisp")
+    (load "math-tests.lisp")
+    (load "misc-tests.lisp")
+
+    (do-tests)))
+
+	
