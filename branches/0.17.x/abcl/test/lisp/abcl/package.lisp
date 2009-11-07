99b3186da8cfdbdb183e1447a8245b526e59f585
ehuelsmann <ehuelsmann@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2009-11-07 11:16:30 +0000

    Rebranch 0.17.
    git-svn-id: http://abcl.org/svn@12259 1c010e3e-69d0-11dd-93a8-456734b0d56f

---
+++ branches/0.17.x/abcl/test/lisp/abcl/package.lisp
@@ -0,0 +1,28 @@
+(defpackage #:abcl.test.lisp 
+  (:use #:cl #:abcl-rt)
+  (:nicknames "ABCL-TEST")
+  (:export #:run))
+(in-package #:abcl.test.lisp)
+
+(defvar *abcl-lisp-test-directory* 
+  (pathname (directory-namestring *load-truename*))
+  "The directory in which the ABCL test source files are located.")
+
+(defun run ()
+  "Run the Lisp test suite for ABCL."
+
+  (let ((*default-pathname-defaults* *abcl-lisp-test-directory*))
+    (rem-all-tests)
+
+    (load "test-utilities.lisp")
+
+    (load "compiler-tests.lisp")
+    (load "condition-tests.lisp")
+    (load "file-system-tests.lisp")
+    (load "java-tests.lisp")
+    (load "math-tests.lisp")
+    (load "misc-tests.lisp")
+
+    (do-tests)))
+
+	
