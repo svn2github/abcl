30069b1554d1393408a0b93f27472d4e1e3093c6
ehuelsmann <ehuelsmann@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2011-03-10 20:06:13 +0000

    Tag 0.25.0.
    git-svn-id: http://abcl.org/svn@13239 1c010e3e-69d0-11dd-93a8-456734b0d56f

---
+++ tags/0.25.0/abcl/test/lisp/abcl/bugs.lisp
@@ -0,0 +1,74 @@
+(in-package :abcl.test.lisp)
+
+;;; When these bugs get fixed, they should be moved elsewhere in the
+;;; testsuite so they remain fixed.
+
+(deftest bugs.logical-pathname.1
+    #|
+Date: Mon, 18 Jan 2010 10:51:07 -0500
+Message-ID: <29af5e2d1001180751l7cf79a3ay929cef1deb9ed063@mail.gmail.com>
+Subject: Re: [armedbear-devel] translate-logical-pathname and :wild-inferiors 
+regression
+From: Alan Ruttenberg <alanruttenberg@gmail.com>
+    |#
+    (progn
+      (setf (logical-pathname-translations "ido") 
+            '(("IDO:IDO-CORE;**;*.*" 
+               "/Users/alanr/repos/infectious-disease-ontology/trunk/src/ontology/ido-core/**/*.*") 
+              ("IDO:IMMUNOLOGY;**;*.*"
+               "/Users/alanr/repos/infectious-disease-ontology/trunk/src/ontology/immunology/**/*.*") 
+              ("IDO:TOOLS;**;*.*" 
+               "/Users/alanr/repos/infectious-disease-ontology/trunk/src/tools/**/*.*") 
+              ("IDO:LIB;**;*.*"
+               "/Users/alanr/repos/infectious-disease-ontology/trunk/lib/**/*.*")))
+      (translate-pathname "IDO:IMMUNOLOGY;" "IDO:IMMUNOLOGY;**;*.*" 
+                          "/Users/alanr/repos/infectious-disease-ontology/trunk/src/ontology/**/*.*"))
+  #P"/users/alanr/repos/infectious-disease-ontology/trunk/src/ontology/")
+
+(deftest bugs.logical-pathname.2
+    #|
+Message-Id: <BBE9D0E5-5166-4D24-9A8A-DC4E766976D1@ISI.EDU>
+From: Thomas Russ <tar@ISI.EDU>
+To: armedbear-devel@common-lisp.net
+Subject: [armedbear-devel] Bug in translate-logical-pathname.
+    |#
+    (progn 
+      (setf (logical-pathname-translations "L")
+            '(("L:NATIVE;**;*.*" "/usr/lisp/abcl/native/**/*.*")))
+      (translate-logical-pathname "L:NATIVE;TEST;FOO.FASL"))
+  #p"/usr/lisp/abcl/native/test/foo.fasl")
+
+      
+(deftest bugs.pathname.1
+    (namestring (make-pathname :directory '(:relative) :name "file" 
+                   :type :unspecific 
+                   :host nil :device nil))
+  "./file")
+
+(deftest bugs.pathname.2
+    (TRANSLATE-PATHNAME 
+     #P"/Users/evenson/work/bordeaux-threads/src/bordeaux-threads.abcl" 
+     #P"/**/**/*.*" 
+     #P"/Users/evenson/.cache/common-lisp/armedbear-0.20.0-dev-darwin-unknown/**/*.*")
+  #P"/Users/evenson/.cache/common-lisp/armedbear-0.20.0-dev-darwin-unknown/bordeaux-threads.abcl")
+
+(deftest bugs.pathname.3  
+    (namestring (MAKE-PATHNAME :HOST NIL :DEVICE NIL 
+                               :DIRECTORY '(:RELATIVE :WILD-INFERIORS) 
+                               :DEFAULTS "/**/"))
+  "**/")
+
+(deftest bugs.java.1
+    (let* ((a (java:jnew-array "byte" 1))
+           (b (let ((array-list (java:jnew (java:jconstructor
+                                       "java.util.ArrayList"))))
+                (java:jcall (java:jmethod "java.util.AbstractList" "add"
+                                          "java.lang.Object")
+                            array-list a)
+                (java:jcall (java:jmethod "java.util.AbstractList" "get" "int")
+                            array-list 0))))
+      (type-of (sys::%make-byte-array-input-stream b)))
+  stream)
+                
+                    
+                    
