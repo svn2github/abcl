3d94abcb55d98cbb6ea8da7d12f0296d61ef9dde
ehuelsmann <ehuelsmann@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2011-08-14 20:57:26 +0000

    Tag 0.26.2.
    git-svn-id: http://abcl.org/svn@13499 1c010e3e-69d0-11dd-93a8-456734b0d56f

---
+++ tags/0.26.2/abcl/contrib/jss/packages.lisp
@@ -0,0 +1,38 @@
+(defpackage :jss
+  (:nicknames "java-simple-syntax" "java-syntax-sucks")
+  (:use :common-lisp :extensions :java)
+  (:export 
+   #:*inhibit-add-to-classpath*
+   #:*added-to-classpath*
+   #:*do-auto-imports*
+
+   #:invoke-restargs
+   #:with-constant-signature
+
+   #:invoke-add-imports
+   #:add-directory-jars-to-class-path
+   #:add-to-classpath
+   #:find-java-class
+   #:need-to-add-directory-jar?
+   #:jcmn
+   #:japropos
+
+;;; Useful utilities to convert common Java items to Lisp counterparts
+   #:hashmap-to-hashtable
+   #:iterable-to-list
+   #:list-to-list
+   #:set-to-list
+   #:vector-to-list
+
+;;; deprecated
+   #:new ; use JAVA:NEW
+   #:get-java-field ; use JAVA:JFIELD
+
+;;; Move to JAVA?
+   #:jclass-all-interfaces
+
+
+;;; Enable compatibility with jss-1.0 by placing symbols in CL-USER
+   #:ensure-compatiblity #:*cl-user-compatibility*)
+   (:shadow #:add-to-classpath))
+
