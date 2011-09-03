4c938352593e04ddbb2e123306285249748552bd
ehuelsmann <ehuelsmann@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2011-09-03 14:29:51 +0000

    Tag 0.27.0.
    git-svn-id: http://abcl.org/svn@13560 1c010e3e-69d0-11dd-93a8-456734b0d56f

---
+++ tags/0.27.0/abcl/contrib/jss/jss.asd
@@ -0,0 +1,20 @@
+;;;; -*- Mode: LISP -*-
+(in-package :asdf)
+
+(defsystem :jss
+  :author "Alan Ruttenberg, Mark Evenson"
+  :version "3.0.1" 
+  :components 
+  ((:module base 
+            :pathname "" :serial t 
+            :components ((:file "packages")
+                         (:file "invoke")
+                         (:file "classpath")
+                         (:file "compat")))))
+
+
+
+
+   
+
+
---
+++ tags/0.27.0/abcl/src/org/armedbear/lisp/mop.lisp
@@ -0,0 +1,73 @@
+;;;; Does not currently include all the MOP, but it should.
+
+(in-package #:mop)
+
+(defclass funcallable-standard-class (class))
+
+(defmethod class-name ((class funcallable-standard-class))
+  'funcallable-standard-class)
+
+;;; StandardGenericFunction.java defines FUNCALLABLE-INSTANCE-FUNCTION and
+;;; SET-FUNCALLABLE-INSTANCE-FUNCTION.
+;;;
+;;; TODO 
+;;;
+;;;   1. Verify that we can make FUNCALLABLE-STANDARD-CLASS instances
+;;;      which work.
+;;;
+;;;   2. Tighten the type checks so that only instances of
+;;;      FUNCALLABLE-STANDARD-CLASS are callable.
+
+(defgeneric validate-superclass (class superclass)
+ (:documentation 
+  "This generic function is called to determine whether the class
+  superclass is suitable for use as a superclass of class."))
+
+;;; TODO Hook VALIDATE-SUPERCLASS into during class metaobject
+;;; initialization and reinitialization. (AMOP p.240-1)
+(defmethod validate-superclass ((class class) (superclass class))
+  (or (eql (class-name superclass) t)
+      (eql (class-name class) (class-name superclass))
+      (or (and (eql (class-name class) 'standard-class)
+               (eql (class-name superclass) 'funcallable-standard-class))
+          (and (eql (class-name class) 'funcallable-standard-class)
+               (eql (class-name superclass) 'standard-class)))))
+
+(export '(funcallable-standard-class
+          validate-superclass
+          direct-slot-definition-class
+          effective-slot-definition-class
+          compute-effective-slot-definition
+          compute-class-precedence-list
+          compute-effective-slot-definition
+          compute-slots
+          finalize-inheritance
+          slot-boundp-using-class
+          slot-makunbound-using-class
+          
+          class-default-initargs
+          class-direct-default-initargs
+          class-direct-slots
+          class-direct-subclasses
+          class-direct-superclasses
+          class-finalized-p
+          class-prototype
+          
+          generic-function-lambda-list
+
+          method-function
+          
+          slot-definition-readers
+          slot-definition-writers
+
+          eql-specializer-object
+          extract-lambda-list
+
+          intern-eql-specializer))
+
+(provide 'mop)
+
+
+
+
+
