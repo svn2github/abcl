1e783137a5b09dcbade52ea5cc0cd0a52242c173
mevenson <mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2012-01-10 23:22:41 +0000

    Bump version to abcl-1.0.2-dev.
    git-svn-id: http://abcl.org/svn@13758 1c010e3e-69d0-11dd-93a8-456734b0d56f

--- branches/1.0.x/abcl/src/org/armedbear/lisp/Version.java
+++ branches/1.0.x/abcl/src/org/armedbear/lisp/Version.java
@@ -41,7 +41,7 @@ public final class Version
 {
   private Version() {}
   
-  static final String baseVersion = "1.0.1";
+  static final String baseVersion = "1.0.2-dev";
   
   static void init() {
     try {
