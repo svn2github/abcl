0275a5c68dab368c377207f9f3d7d7f094d5e729
mevenson <mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2012-12-03 04:49:08 +0000

    1.1.x: now produces abcl-1.1.0-rc-2 artifacts.
    git-svn-id: http://abcl.org/svn@14283 1c010e3e-69d0-11dd-93a8-456734b0d56f

--- branches/1.1.x/src/org/armedbear/lisp/Version.java
+++ branches/1.1.x/src/org/armedbear/lisp/Version.java
@@ -41,7 +41,7 @@ public final class Version
 {
   private Version() {}
   
-  static final String baseVersion = "1.1.0-rc-1";
+  static final String baseVersion = "1.1.0-rc-2";
   
   static void init() {
     try {
