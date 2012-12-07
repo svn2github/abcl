f32f3fe43a77d5b816728f6d0d8f914984399337
mevenson <mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2012-12-07 12:23:36 +0000

    1.1.0:  Initiate version bump in minimal manner.
    git-svn-id: http://abcl.org/svn@14310 1c010e3e-69d0-11dd-93a8-456734b0d56f

--- branches/1.1.x/src/org/armedbear/lisp/Version.java
+++ branches/1.1.x/src/org/armedbear/lisp/Version.java
@@ -41,7 +41,7 @@ public final class Version
 {
   private Version() {}
   
-  static final String baseVersion = "1.1.0-rc-3";
+  static final String baseVersion = "1.1.0";
   
   static void init() {
     try {
