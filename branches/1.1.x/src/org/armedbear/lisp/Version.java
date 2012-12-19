5c7644fc8fb00b3e650dd44deb73a817511f0b05
mevenson <mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2012-12-19 09:49:22 +0000

    1.1.x:  now build abcl-1.1.1-dev artifacts.
    git-svn-id: http://abcl.org/svn@14340 1c010e3e-69d0-11dd-93a8-456734b0d56f

--- branches/1.1.x/src/org/armedbear/lisp/Version.java
+++ branches/1.1.x/src/org/armedbear/lisp/Version.java
@@ -41,7 +41,7 @@ public final class Version
 {
   private Version() {}
   
-  static final String baseVersion = "1.1.0.2";
+  static final String baseVersion = "1.1.1-dev";
   
   static void init() {
     try {
