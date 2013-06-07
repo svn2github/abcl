d7d67cb63db27f0af8f1fd89e6cad62c3505be5c
mevenson <mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2013-06-07 12:00:23 +0000

    [mq]: abcl-1.2.0.diff
    git-svn-id: http://abcl.org/svn@14523 1c010e3e-69d0-11dd-93a8-456734b0d56f

--- branches/1.2.x/src/org/armedbear/lisp/Version.java
+++ branches/1.2.x/src/org/armedbear/lisp/Version.java
@@ -41,7 +41,7 @@ public final class Version
 {
   private Version() {}
   
-  static final String baseVersion = "1.2.0-rc-1";
+  static final String baseVersion = "1.2.0";
   
   static void init() {
     try {
