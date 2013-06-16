a46a1eae3d5c37eae2c78dff8cad7f469bd9c8c2
ehuelsmann <ehuelsmann@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2013-06-16 21:56:13 +0000

    Increment version number, taking recent fixes into account.
    git-svn-id: http://abcl.org/svn@14546 1c010e3e-69d0-11dd-93a8-456734b0d56f

--- branches/1.2.x/src/org/armedbear/lisp/Version.java
+++ branches/1.2.x/src/org/armedbear/lisp/Version.java
@@ -41,7 +41,7 @@ public final class Version
 {
   private Version() {}
   
-  static final String baseVersion = "1.2.1-rc-0";
+  static final String baseVersion = "1.2.1-rc-2";
   
   static void init() {
     try {
