e63a1c3015b9476c8e7368a7f70866e1fa32eb50
mevenson <mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2014-04-22 11:27:52 +0000

    branches/1.3.1 now builds 1.3.1-rc-2.
    git-svn-id: http://abcl.org/svn@14692 1c010e3e-69d0-11dd-93a8-456734b0d56f

--- branches/1.3.1/src/org/armedbear/lisp/Version.java
+++ branches/1.3.1/src/org/armedbear/lisp/Version.java
@@ -41,7 +41,7 @@ public final class Version
 {
   private Version() {}
   
-  static final String baseVersion = "1.3.1-rc-1";
+  static final String baseVersion = "1.3.1-rc-2";
   
   static void init() {
     try {
